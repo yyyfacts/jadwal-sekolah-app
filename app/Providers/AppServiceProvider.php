9a2d681f6e0d2780cf78243b7e1e5ee9fcce2a57
Zain Aufa <zainaufa99@gmail.com>
2025-12-17 12:38:28 +0700

    Force HTTPS schema

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <--- 1. TAMBAHKAN BARIS INI DI ATAS
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // --- 2. TAMBAHKAN KODE INI DI DALAM BOOT ---
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
